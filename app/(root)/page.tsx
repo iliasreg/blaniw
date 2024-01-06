d478aad339e6d35d149dd21cda1383a24e8d1297
ILIAS REGUIG <iliasreg18@gmail.com>
2024-01-06 01:08:38 +0100

    final version

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -36,8 +36,8 @@ export default async function Home() {
       <section id='events' className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
         <h2 className='h2-bold'>Trust by <br /> Thousands of Events</h2>
         <div className='flex flex-col w-full gap-5 md:flex-row'>
-          Search
-          CategoryFilter
+          {/* Search
+          CategoryFilter */}
         </div>
 
         <Collection 
